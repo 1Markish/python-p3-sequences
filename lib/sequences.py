33fac70a0978153a0762ada9342d45ad42e1ddc7
1Markish <mark.kipchumba@student.moringaschool.com>
2023-08-21 11:36:11 +0300

    Done with assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+lenth= 10
 
 def print_fibonacci(length):
-    pass
+      if length == 0 :
+          print([])
+      elif length == 1 :
+          print([0])
+      elif length == 2 :
+          print([0,1])
+      else :
+          s=print_fibonacci(length - 1 )
+          s.append(s[-1] + s[-2]) ;   print(s)  
